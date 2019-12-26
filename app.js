a76a364a96c627979f8d0c8833a88836f75e3cf8
Fernando <fernandomellone2@gmail.com>
2019-12-26 18:10:43 -0300

    se conectó con la base de datos de firebase

---
+++ app.js
@@ -0,0 +1,34 @@
+console.log("conectado");
+
+firebase.initializeApp({
+    apiKey: "AIzaSyB5lRiA2zsT55k14mB95eyGQZDAAB_WmqE",
+    authDomain: "linkstart-1.firebaseapp.com",
+    projectId: "linkstart-1",
+});
+var db = firebase.firestore();
+
+var nombre = document.getElementById('nombre').value;
+var email = document.getElementById('email').value;
+var celular = document.getElementById('celular').value;
+var comentario = document.getElementById('comentario').value;
+
+function guardarInfo() {
+
+    db.collection("personas").add({
+            nombre: nombre,
+            email: email,
+            celular: celular,
+            comentario: comentario
+        })
+        .then(
+            function(docRef) {
+                console.log("Document written with ID: ", docRef.id);
+                document.getElementById('nombre').value = "";
+                document.getElementById('email').value = "";
+                document.getElementById('celular').value = "";
+                document.getElementById('comentario').value = "";
+            })
+        .catch(function(error) {
+            console.error("Error adding document: ", error);
+        });
+}
