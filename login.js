d88245c362cdef0b9d5e6ad84c9ced9fc6b913cd
nandyme <fernandomellone2@gmail.com>
2019-12-27 00:21:07 -0300

    se puede iniciar y cerrar sesión

--- login.js
+++ login.js
@@ -20,15 +20,39 @@ function login() {
   });
 }
 
+function logout() {
+  firebase.auth().signOut().then(function() {
+    console.log("Sign-out successful.");
+    location.reload();
+  }).catch(function(error) {
+    // An error happened.
+  });
+  
+}
+
 function observador() {
   document.getElementById('user').value = "";
   document.getElementById('pass').value ="";
   firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
       console.log("Conectado");
+      var btn = document.createElement("BUTTON");
+      btn.innerHTML = "Log Out";
+      btn.id = "btn-logout";
+      btn.setAttribute("onclick","logout()");
+      document.getElementById("loginContainer").removeChild(document.getElementById("form"));
+      document.getElementById("loginContainer").appendChild(btn);
+      // document.getElementById("btn-logout").style.display = "block";
+      // document.getElementById("btn-logout").style.margin = "1rem auto";
     }
     else {
-      console.log("Desconectado")
+      console.log("Desconectado");
+      document.getElementById("form").style.display = "block";
+      // var btnin = document.createElement("BUTTON");
+      // btnin.innerHTML = "Log In";
+      // btnin.id = "btn-login";
+      // btnin.setAttribute("onclick","login()");
+      // document.getElementById("btn-login").appendChild(btnin);
     }
   });
 }
