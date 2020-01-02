1ec5c0d69eb12995c823cfb4c5762105f7c64797
Fernando <fernandomellone2@gmail.com>
2020-01-02 10:14:48 -0300

    otros cambios

--- js/read_donaciones.js
+++ js/read_donaciones.js
@@ -1,35 +1,25 @@
 //se inicializa firebase
 var db = firebase.firestore();
 
-// setTimeout(function() {
-//     console.log(total);
-//     new Vue({
-//         el: '#app',
-
-//         data() {
-//             return {
-//                 acumulado: total
-//             }
-//         }
-//     })
-// }, 0);
 
 //Se crea una variable que va a contener el total de los montos
 var total = 0;
 
 //Se extrae los montos del firestore cloud y se suman en la variable "total"
 
-db.collection("donaciones").get().then(function(querySnapshot) {
-    querySnapshot.forEach(function(doc) {
-        total += doc.data()['monto'];
+db.collection("donaciones").get()
+    .then(function(querySnapshot) {
+        querySnapshot.forEach(function(doc) {
+            total += doc.data()['monto'];
+        });
     });
-    new Vue({
-        el: '#app',
 
-        data() {
-            return {
-                acumulado: total
-            }
+new Vue({
+    el: '#app',
+
+    data() {
+        return {
+            acumulado: total
         }
-    })
-});
+    }
+})
